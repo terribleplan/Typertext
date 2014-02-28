6aa6cb86733879748269f4396f1627f5c5f12155
Kegan Myers <kegan@keganmyers.com>
2014-02-28 10:59:54 -0600

    Document HttpResponse

--- lib/Typertext/Http/HttpResponse.ts
+++ lib/Typertext/Http/HttpResponse.ts
@@ -1,5 +1,24 @@
+/**
+ * @namespace   Typertext
+ * @module      Http
+ */
 module Typertext.Http {
     export class HttpResponse extends Typertext.GenericResponse<string> {
+        /**
+         * A class for passing the response around as a raw string
+         *
+         * @class   HttpResponse
+         * @extends GenericResponse
+         *
+         * @param   {HttpResponseStatus}    status
+         * @param   {HttpHeaderData}        responseHeaders
+         * @param   {number}                httpResponseCode
+         * @param   {string}                responseBody
+         *
+         * @author      Kegan Myers <kegan@keganmyers.com>
+         * @version     0.3.0
+         * @constructor
+         */
         constructor(status:HttpResponseStatus, responseHeaders?:HttpHeaderData, httpResponseCode?:number, responseBody?:string) {
             super(status, responseHeaders, httpResponseCode, responseBody);
         }
