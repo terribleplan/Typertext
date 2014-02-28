d15b8a9149d6c192cc3cfa13b5e15576937c0965
Kegan Myers <kegan@keganmyers.com>
2014-02-28 10:37:09 -0600

    Document BaseException

--- lib/Typertext/BaseException.ts
+++ lib/Typertext/BaseException.ts
@@ -1,23 +1,51 @@
+/**
+ * @module      Typertext
+ * @submodule   Http
+ * @submodule   Json
+ */
 module Typertext {
     export class BaseException<T> {
         private code:number;
         private custom:T;
         private message:string;
 
+        /**
+         * A simple utility class to provide a defined way to pass exceptions occurring within Typertext
+         *
+         * @param {string}  message
+         * @param {number}  code
+         * @param {T}       custom
+         * @constructor
+         */
         constructor(message:string, code:number, custom:T) {
             this.message = message;
             this.code = code;
             this.custom = custom;
         }
 
+        /**
+         * Accessor method
+         *
+         * @returns {number}
+         */
         public GetCode():number {
             return this.code;
         }
 
+        /**
+         * Accessor method
+         *
+         * @returns {string}
+         */
         public GetMessage():string {
             return this.message;
         }
 
+        /**
+         * Accessor method
+         *
+         * @returns {T}
+         */
         public GetCustom():T {
             return this.custom;
         }
