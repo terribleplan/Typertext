e9cf9f5f175125b0a22e7645ea40b726ed2c84fa
Kegan Myers <kegan@keganmyers.com>
2014-03-03 14:47:49 -0600

    Add test for HttpResponse

--- test/Typertext/Http/HttpException.test.js
+++ test/Typertext/Http/HttpException.test.js
@@ -6,8 +6,8 @@ describe("Typertext.Http.HttpException", function () {
     it("works according to the parent class", function () {
         var inputString = "Test message",
             inputCode = 239,
-            inputHttp = Typertext.Http.HttpResponseStatus.clientError;
-        var testClass = new Typertext.Http.HttpException(inputString, inputCode, inputHttp);
+            inputHttp = Typertext.Http.HttpResponseStatus.clientError,
+            testClass = new Typertext.Http.HttpException(inputString, inputCode, inputHttp);
 
         expect(testClass.GetCode()).toEqual(inputCode);
         expect(testClass.GetCustom()).toEqual(inputHttp);
--- test/Typertext/Http/HttpResponse.test.js
+++ test/Typertext/Http/HttpResponse.test.js
@@ -1,16 +1,20 @@
-describe("", function () {
+describe("Typertext.Http.HttpResponse", function () {
     it("exists", function () {
-        expect(typeof Typertext.Http.HttpException).toEqual("function");
+        expect(typeof Typertext.Http.HttpResponse).toEqual("function");
     });
 
     it("works according to the parent class", function () {
+        function hf(input) {
+            return "TestString" + input;
+        }
         var inputString = "Test message",
             inputCode = 239,
-            inputHttp = Typertext.Http.HttpResponseStatus.clientError;
-        var testClass = new Typertext.Http.HttpException(inputString, inputCode, inputHttp);
+            inputHttp = Typertext.Http.HttpResponseStatus.clientError,
+            testClass = new Typertext.Http.HttpResponse(inputHttp, hf, inputCode, inputString);
 
-        expect(testClass.GetCode()).toEqual(inputCode);
-        expect(testClass.GetCustom()).toEqual(inputHttp);
-        expect(testClass.GetMessage()).toEqual(inputString);
+        expect(testClass.GetContent()).toEqual(inputString);
+        expect(testClass.GetHeader("foo")).toEqual("TestStringfoo");
+        expect(testClass.GetHttpStatus()).toEqual(inputCode);
+        expect(testClass.GetStatus()).toEqual(inputHttp);
     });
 });
