c98f357bbad78cfa26b42d9cb4735a261aab6e96
Kegan Myers <kegan@keganmyers.com>
2014-03-03 14:40:20 -0600

    Add test for JsonResponse

---
+++ test/Typertext/Http/HttpException.test.js
@@ -0,0 +1,16 @@
+describe("", function () {
+    it("exists", function () {
+        expect(typeof Typertext.Http.HttpException).toEqual("function");
+    });
+
+    it("works according to the parent class", function () {
+        var inputString = "Test message",
+            inputCode = 239,
+            inputHttp = Typertext.Http.HttpResponseStatus.clientError;
+        var testClass = new Typertext.Http.HttpException(inputString, inputCode, inputHttp);
+
+        expect(testClass.GetCode()).toEqual(inputCode);
+        expect(testClass.GetCustom()).toEqual(inputHttp);
+        expect(testClass.GetMessage()).toEqual(inputString);
+    });
+});
